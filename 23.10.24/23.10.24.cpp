f387c7763f3d7bd5fcd8549f64da1a6bd6c21e49
CODDY 15 <CODDY 15@DESKTOP-31OPKHL>
2024-10-23 15:33:11 +0200

    Добавьте файлы проекта.

---
+++ 23.10.24/23.10.24.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    struct Human {
+        string name;
+        int age;
+        string prof;
+    };
+
+    Human my_struct;
+    my_struct.age = 50;
+    cout << my_struct.age << endl;
+
+
+
+
+    
+  
+    return 0;
+
+}
